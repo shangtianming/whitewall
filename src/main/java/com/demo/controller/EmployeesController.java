48af6a46469548162ed3722d5059759eafef7957
whitewall <yin921125@163.com>
2024-02-25 11:58:26 +0800

    更新注释

--- src/main/java/com/demo/controller/EmployeesController.java
+++ src/main/java/com/demo/controller/EmployeesController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/emp")
 public class EmployeesController  {
-
+    // Autowired：自动装配，无需手动创建对象。默认类型（byType）查找
+    // 如果存在相同类型，可以配合byname的注解@Qualifier来避免
     @Autowired
     EmployeeService employeeService;
 
